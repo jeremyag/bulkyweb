8ca9771296ac2dba9b8822ce0daf0ceba2cb20f3
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2024-02-03 16:32:03 +0800

    Feature/categories (#2)
    * Recreated the Categories Crud in Razor Project.
    
    * Updated bootstrap template and adjust form designs in category.
    
    * Added different projects.
    
    * Created category repository.
    
    * Created UnitOfWork.
    
    * Created Customer and Admin areas.

---
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,102 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var objCategoryList = _unitOfWork.Category.GetAll().ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save();
+
+                TempData["Success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var objCategory = _unitOfWork.Category.Get(u => u.Id == id);
+            if (objCategory == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(objCategory);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save();
+
+                TempData["Success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var objCategory = _unitOfWork.Category.Get(u => u.Id == id);
+            if (objCategory == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(objCategory);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            var objCategory = _unitOfWork.Category.Get(u => u.Id == id);
+            if (objCategory == null) { return NotFound(); }
+
+            _unitOfWork.Category.Remove(objCategory);
+            _unitOfWork.Save();
+
+            TempData["Success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
