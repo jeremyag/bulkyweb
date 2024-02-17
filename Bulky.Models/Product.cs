a400fca2ac18949120fedc2bada4bea2964928b3
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2024-02-17 16:26:26 +0800

    Update Products Table with foreign key and Image Url

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Bulky.Models;
 
@@ -33,4 +34,8 @@ public class Product
     [Display(Name = "Price for 100+")]
     [Range(1, 1000)]
     public double Price100 { get; set; }
+    public int CategoryId { get; set; }
+    [ForeignKey("CategoryId")]
+    public Category Category { get; set; }
+    public string ImageUrl { get; set; }
 }
