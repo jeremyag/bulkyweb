2fbeae1632bb269ba27fbe6c4bc23ae7fe762862
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2024-04-01 16:17:05 +0800

    Modify register and login form

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -18,6 +18,13 @@ builder.Services
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
+
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
