7eb071f226fca018fa506bc576f08aec61f5b9db
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2024-04-02 17:44:25 +0800

    Added shopping cart page.

--- Bulky.DataAccess/Repository/Repository.cs
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -46,9 +46,14 @@ public class Repository<T> : IRepository<T> where T : class
 
     }
 
-    public IEnumerable<T> GetAll(string? includeProperties = null)
+    public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
     {
         IQueryable<T> query = dbSet;
+        if(filter != null)
+        {
+            query = query.Where(filter);
+        }
+
         if (!string.IsNullOrEmpty(includeProperties))
         {
             foreach(var includeProp in includeProperties
